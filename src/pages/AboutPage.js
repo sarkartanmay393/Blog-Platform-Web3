038aa92ae15253abcb591ccc62f92ee5a87174e6
Tanmay Sarkar <sarkartanmay393@gmail.com>
2022-12-26 20:44:09 +0530

    Phase: Added all simple components
    It is ready to visit, but this is still early.

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,6 +1,21 @@
+const GitHubIcon = `https://cdn-icons-png.flaticon.com/512/25/25231.png`;
+const HauraFunction = () => {
+    let GithubCardEl = document.getElementById('github-card-el');
+};
+
 const AboutPage = function () {
     return (
-        <div>This is the AboutPage</div>
+        <div className='container'>
+            <h1 onClick={ HauraFunction}>Want to know about me ?</h1>
+            <div id ='github-card-el' className='card'>
+                <a id='github-card-link' href='https://github.com/sarkartanmay393' target='_blank'>
+                    <div className='row'>
+                        <img id='github-icon-on-card' src={GitHubIcon} width='38' alt='github icon' />
+                        <h2>/sarkartanmay393</h2>
+                    </div>
+                </a>
+            </div>
+        </div>
     );
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,39 @@
-const HomePage = function () {
-    return (
-        <div>This is the homepage</div>
-    );
-}
+import {Link} from "react-router-dom";
 
+const ImageLink = `http://cdn.onlinewebfonts.com/svg/img_266735.png`;
+const ExploreBlogsButton = () => {
+    window.open("")
+
+};
+
+const HomePage = () => (
+    <div className="container">
+        <img id='home-page-icon' src={ImageLink} width='35' alt='mimic icon to ANOTHER' />
+        <h1>Decentralized Blogging Platform</h1>
+        <p>
+            Hey fellows, this is where we post blogs that uses blockchain technology to
+            decentralises itself, and give ownership to everybody on their blogs and
+            uncensored flow in go. Decentralised blogging platforms have several advantages
+            over traditional, centralised platforms. Because they are not controlled by
+            a single company, they are less vulnerable to censorship or manipulation.
+        </p>
+        <p>
+            A decentralised blogging platform is a type of online platform that allows
+            users to create and publish blog posts, but is not controlled by a central
+            authority. Instead, the platform is distributed across a network of computers,
+            and decisions about the platform's content and operation are made by the community
+            of users rather than a single entity.
+        </p>
+        <p>
+            One example of a decentralised blogging platform is Steemit, which uses a
+            blockchain-based system to store and manage content. Users on Steemit can
+            earn cryptocurrency by creating and curating high-quality content,
+            incentivizing the creation of engaging and informative blog posts. Other
+            decentralised blogging platforms include Scuttlebutt and Mastodon, which
+            use different underlying technologies but have similar decentralized structures.
+        </p>
+
+        <button><Link id= 'explore-link' to='/articles'>Explore Blogs</Link></button>
+    </div>
+);
 export default HomePage;
